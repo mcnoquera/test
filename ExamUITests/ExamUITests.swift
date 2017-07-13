61f563d3bcc9588b78bfc03109632617121c2e7e
Mark Angelo Noquera <mnoquera@exist.com>
2017-07-13 22:02:43 +0800

    schema

--- ExamUITests/ExamUITests.swift
+++ ExamUITests/ExamUITests.swift
@@ -32,5 +32,5 @@ class ExamUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
 }
---
+++ Pods/Target Support Files/Pods-ExamTests/Pods-ExamTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExamTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExamTests
+@end
